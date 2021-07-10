c15e54057805e203b80b56754bd66851c28c6b3f
ChoiJunYeong <hlnsud12@gist.ac.kr>
2021-07-11 02:04:06 +0900

    [FEAT] init typescript

---
+++ routes/upload/upload.ts
@@ -0,0 +1,33 @@
+import * as createError from "http-errors"
+import express from 'express';
+
+import ocrRouter from './ocr'
+
+var app = express();
+
+var router = express.Router();
+
+router.use('/OCR', ocrRouter);
+
+// catch 404 and forward to error handler
+router.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+router.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'OCR' });
+});
+
+module.exports = router;
