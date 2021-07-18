2362d9a08838e25fc2731d86997343bc1842f382
NorangBerry <hlnsud12@naver.com>
2021-07-18 23:47:10 +0900

    [TEST] add tests for OCR/source

--- test/test.ts
+++ test/test.ts
@@ -9,10 +9,24 @@ import app from '../src/app'
 //   });
 // }); 
 
-describe('upload', function() {
+describe('upload source', function() {
     it('valid file', function(done) {
         supertest(app).post('/upload/OCR/source')
             // .send({data:'x'})
+            .attach('source', 'test/test_img.png')
+            .expect(200)
+            .end(function(err:Error, res:supertest.Response) {
+                if (err) return done(err);
+                done();
+            });
+              // .field('extra_info', '{"in":"case you want to send json along with your file"}')
+    });
+    it('multiple file', function(done) {
+        supertest(app).post('/upload/OCR/source')
+            // .send({data:'x'})
+            .attach('source', 'test/test_img.png')
+            .attach('source', 'test/test_img.png')
+            .attach('source', 'test/test_img.png')
             .attach('source', 'test/test_img.png')
             .expect(200)
             .end(function(err:Error, res:supertest.Response) {
@@ -27,6 +41,20 @@ describe('upload', function() {
             .attach('source', 'test/test_txt.txt')
             .expect(415)
             .end(function(err, res) {
+                if (err) return done(err);
+                done();
+            });
+              // .field('extra_info', '{"in":"case you want to send json along with your file"}')
+    });
+    it('valid and invalid file mix', function(done) {
+        supertest(app).post('/upload/OCR/source')
+            // .send({data:'x'})
+            .attach('source', 'test/test_img.png')
+            .attach('source', 'test/test_txt.txt')
+            .attach('source', 'test/test_img.png')
+            .attach('source', 'test/test_txt.txt')
+            .expect(415)
+            .end(function(err:Error, res:supertest.Response) {
                 if (err) return done(err);
                 done();
             });
