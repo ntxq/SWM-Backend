5f7a37cef273c389f47018edc94db8342a28de6f
NorangBerry <hlnsud12@naver.com>
2021-07-19 23:12:06 +0900

    [FEAT] save blank image

--- src/routes/upload/segmentation.ts
+++ src/routes/upload/segmentation.ts
@@ -6,41 +6,72 @@ import { req_ocr_result } from 'src/modules/req_ai_server';
 import { mysql_connection, Procedure } from 'src/sql/sql_connection';
 import { grpcSocket } from 'src/gRPC/grcp_socket';
 
-import { Request, Response } from 'express-serve-static-core'
+import { Request, Response, NextFunction } from 'express-serve-static-core';
 import { multer_image } from 'src/routes/multer_options';
+import createError from 'http-errors';
 
 var router = express.Router();
 
 router.post('/source', multer_image.array('source'), (req:Request,res:Response) => {
-	try{
-		const req_id_map = new Map<string,number>();
-		//todo 최준영 제대로 된 user id 로 변환
-		const user_id = 123123123
-		const files = req.files as Express.Multer.File[];
-		const procedures = Array<Procedure>();
-		for(var i =0;i<files.length;i++){
-			const file = files[i]
-			const procedure:Procedure = {
-				query:'sp_add_original_source',
-				parameters:[user_id,file.originalname],
-				callback:(rows:any,err:any)=>{
-					const req_id = rows['id']
-					const old_path = file.path
-					const new_path = `${IMAGE_DIR}/original/${req_id}${path.extname(file.originalname)}`
-					fs.promises.rename(old_path, new_path)
-					req_id_map.set(file.originalname,req_id)
-				}
+	const req_id_map = new Map<string,number>();
+	//todo 최준영 제대로 된 user id 로 변환
+	const user_id = 123123123
+	const files = req.files as Express.Multer.File[];
+	const procedures = Array<Procedure>();
+	for(var i =0;i<files.length;i++){
+		const file = files[i]
+		const procedure:Procedure = {
+			query:'sp_add_original_source',
+			parameters:[user_id,file.originalname],
+			callback:(rows:any,err:any)=>{
+				const req_id = rows['id']
+				const old_path = file.path
+				const new_path = `${IMAGE_DIR}/original/${req_id}${path.extname(file.originalname)}`
+				fs.promises.rename(old_path, new_path)
+				req_id_map.set(file.originalname,req_id)
 			}
-			procedures.push(procedure)
 		}
-		mysql_connection.callMultipleProcedure(procedures,(err:any,result:any)=>{
-			res.send({req_ids:Object.fromEntries(req_id_map)})
-		})
+		procedures.push(procedure)
 	}
-	catch(e){
-		res.status(500).send({msg:"internel error"});
-	}
+	mysql_connection.callMultipleProcedure(procedures,(err:any,result:any)=>{
+		res.send({req_ids:Object.fromEntries(req_id_map)})
+	})
 });
+
+router.post('/blank', multer_image.array('blank'), (req:Request,res:Response,next:NextFunction) => {
+	//todo 최준영 제대로 된 user id 로 변환
+	const user_id = 123123123
+	const map_ids:number[] = JSON.parse(req.body.map_ids);
+	const files = req.files as Express.Multer.File[];
+	const req_id_map = new Map<string,number>();
+	const procedures = Array<Procedure>();
+	for(var i =0;i<map_ids.length;i++){
+		const req_id = map_ids[i]
+		const blank_file = files[i];
+		const procedure:Procedure = {
+			query:'sp_set_balnk_source',
+			parameters:[req_id,user_id,blank_file.originalname],
+			callback:(rows:any,err:any)=>{
+				if(err){
+					res.status(400);
+					return;
+				}
+				const old_path = blank_file.path
+				const new_path = `${IMAGE_DIR}/blank/${req_id}${path.extname(blank_file.originalname)}`
+				fs.promises.rename(old_path, new_path)
+				req_id_map.set(blank_file.originalname,req_id)
+			}
+		}
+		procedures.push(procedure)
+	}
+	mysql_connection.callMultipleProcedure(procedures,(err:any,result:any)=>{
+		if(err){
+			next(createError(res.statusCode));
+			return;
+		}
+		res.send({req_ids:Object.fromEntries(req_id_map)})
+	})
+})
 
 router.get('/result', (req:Request,res:Response) => {
 	try{
