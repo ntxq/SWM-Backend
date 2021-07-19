f4b9846dab05a9c1f6a46ae9a683fc0e38fd2697
NorangBerry <hlnsud12@naver.com>
2021-07-19 18:20:16 +0900

    [FEAT] image-req_id one-to-one mapping

--- src/routes/upload/ocr.ts
+++ src/routes/upload/ocr.ts
@@ -1,18 +1,14 @@
 import express from 'express';
 import multer, { FileFilterCallback } from 'multer';
 import fs from 'fs';
-import { isImageFile, generate_id } from 'src/modules/utils';
+import { isImageFile } from 'src/modules/utils';
 import { IMAGE_DIR } from 'src/modules/const';
 import path from 'path';
 import { req_now_step, req_ocr_result, save_ocr_result } from 'src/modules/req_ai_server';
-import { mysql_connection } from 'src/sql/sql_connection';
+import { mysql_connection, Procedure } from 'src/sql/sql_connection';
 import { grpcSocket } from 'src/gRPC/grcp_socket';
 
-interface ImageRequest extends Request{
-	name?:string;
-}
-
-import { Request, Response, NextFunction } from 'express-serve-static-core'
+import { Request, Response } from 'express-serve-static-core'
 
 var router = express.Router();
 
@@ -28,14 +24,11 @@ var storage = multer.diskStorage({
 	}
 })
 
-var fileFilter = function(req:ImageRequest, file:Express.Multer.File, cb:FileFilterCallback){
+var fileFilter = function(req:Request, file:Express.Multer.File, cb:FileFilterCallback){
 	if(!isImageFile(file)){
 		(req.res as Response).statusCode = 415
 		// req.h = 'goes wrong on the mimetype';
 		cb(Error('Error: Images Only!'));
-	}
-	if(!req.name){
-		req.name = file.originalname
 	}
 	return cb(null,true)
 }
@@ -43,22 +36,31 @@ var fileFilter = function(req:ImageRequest, file:Express.Multer.File, cb:FileFil
 const upload = multer({ storage: storage, fileFilter:fileFilter, limits: { fileSize: 1024 * 1024 * 1024 } });
 
 
-router.post('/source', upload.array('source'), (req:ImageRequest,res:Response) => {
+router.post('/source', upload.array('source'), (req:Request,res:Response) => {
 	try{
+		const req_id_map = new Map<string,number>();
 		//todo 최준영 제대로 된 user id 로 변환
-		const user_id = 'test_user_junyeong'
+		const user_id = 123123123
 		const files = req.files as Express.Multer.File[];
-		mysql_connection.callProcedure('sp_add_original_source',[user_id,files.length,req.name],async (rows: any)=>{
-			const req_id = rows['id']
-			for (var i = 0; i < files.length;i++) {
-				const file = files[i]
-				const old_path = file.path
-				const new_path = `${IMAGE_DIR}/original/${req_id}_${i}${path.extname(file.originalname)}`
-				await fs.promises.rename(old_path, new_path)
-			};
-			grpcSocket.StartOCR(req_id,files.length)
-			res.send({req_id:req_id})
-		},()=>{})
+		const procedures = Array<Procedure>();
+		for(var i =0;i<files.length;i++){
+			const file = files[i]
+			const procedure:Procedure = {
+				query:'sp_add_original_source',
+				parameters:[user_id,file.originalname],
+				callback:(rows:any,err:any)=>{
+					const req_id = rows['id']
+					const old_path = file.path
+					const new_path = `${IMAGE_DIR}/original/${req_id}${path.extname(file.originalname)}`
+					fs.promises.rename(old_path, new_path)
+					req_id_map.set(file.originalname,req_id)
+				}
+			}
+			procedures.push(procedure)
+		}
+		mysql_connection.callMultipleProcedure(procedures,(err:any,result:any)=>{
+			res.send({req_id_map:Object.fromEntries(req_id_map)})
+		})
 	}
 	catch(e){
 		res.status(500).send({msg:"internel error"});
