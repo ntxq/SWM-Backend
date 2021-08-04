152e27c06df8892a332e4309f9fcf532ba0cc0e8
ntxq <bkwak@ust.hk>
2021-08-04 15:42:05 +0900

    Initial frontend deployment

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,53 @@
-import createError from 'http-errors'
-import express from 'express'
-import path from 'path'
-import cookieParser = require('cookie-parser')
-import cors = require("cors")
-import logger = require('morgan')
-import { Request, Response, NextFunction } from 'express-serve-static-core'
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser = require("cookie-parser");
+import cors = require("cors");
+import logger = require("morgan");
+import { Request, Response, NextFunction } from "express-serve-static-core";
 
-var uploadRouter = require('./routes/upload/upload.ts');
+var uploadRouter = require("./routes/upload/upload.ts");
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json({limit : "50mb"}));
-app.use(express.urlencoded({ extended: false, limit : "50mb" }));
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-app.use('/upload', uploadRouter);
+app.use(
+  "/static",
+  express.static(path.join(__dirname, "../frontend/build/static"))
+);
+
+app.use("/upload", uploadRouter);
+app.get("*", (req: Request, res: Response) =>
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "../frontend/build/"),
+  })
+);
 
 // catch 404 and forward to error handler
-app.use(function(req:Request, res:Response, next:NextFunction) {
+app.use(function (req: Request, res: Response, next: NextFunction) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err:createError.HttpError, req:Request, res:Response, next:NextFunction) {
-  console.log(err.message)
+app.use(function (
+  err: createError.HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log(err.message);
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.send('error');
+  res.send("error");
 });
 
 export default app;
