e4b03043f3ab68e4408b3cda884e2fa1789418aa
ntxq <bkwak@ust.hk>
2021-09-02 22:09:24 +0800

    Resolve kakao oauth credential issue

--- src/routes/kakao.ts
+++ src/routes/kakao.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import axios from "axios";
-import { Request, Response, NextFunction } from "express-serve-static-core";
+import { Request, Response } from "express-serve-static-core";
 import { asyncRouterWrap } from "src/modules/utils";
+import QueryString from "qs";
 
 const kakaoRouter = express.Router();
 
@@ -17,21 +18,27 @@ type TokenResponse = {
 const REDIRECT_URL = "http://localhost:3000/oauth";
 
 async function getKakaoToken(authCode: string): Promise<string> {
-  const token = await axios
-    .post(
-      "https://kauth.kakao.com/oauth/token",
-      {
-        grant_type: "authorization_code",
-        client_id: "4ee38a9230dc15bb654d1a79386e0e7c", //env에서 가져오기
-        redirect_uri: REDIRECT_URL,
-        code: authCode,
-      },
-      {
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      }
-    )
+  const token = await axios({
+    method: "POST",
+    url: "https://kauth.kakao.com/oauth/token",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+
+    //JSON.stringyfy !== QueryString.stringify
+    data: QueryString.stringify({
+      grant_type: "authorization_code",
+      client_id: "4ee38a9230dc15bb654d1a79386e0e7c", //env에서 REST_API_KEY 받아오기
+      redirect_uri: REDIRECT_URL,
+      code: authCode,
+    }),
+  })
     .then((response) => response.data as TokenResponse)
-    .then((data) => data.access_token);
+    .then((data) => data.access_token) //필요하면 refresh_token 사용하기
+    .catch((error) => {
+      console.log(error); //카카오 에러코드 확인용
+      return "";
+    });
 
   return token;
 }
@@ -41,10 +48,10 @@ kakaoRouter.get(
   asyncRouterWrap(async (request: Request, response: Response) => {
     //authCode는 따로 저장할 필요가 없음
     const authCode = request.query.code as string;
-    const token = await getKakaoToken(authCode);
+    const token = await getKakaoToken(authCode); //token DB에 넣고 기타등등
 
     response.cookie("kakao_token", token);
-    response.redirect("../");
+    response.redirect("/home");
   })
 );
 
