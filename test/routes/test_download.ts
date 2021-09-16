c00b4442652174c252bc9b0e262ef874e9968c74
NorangBerry <hlnsud12@gist.ac.kr>
2021-09-17 01:20:41 +0900

    [TEST] add download test

---
+++ test/routes/test_download.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable no-prototype-builtins */
+import supertest from "supertest";
+import app from "src/app";
+import { expect } from "chai";
+import sinon from "ts-sinon";
+import { queryManager } from "src/sql/mysql_connection_manager";
+import { s3 } from "src/modules/s3_wrapper";
+
+describe("/api/download", function () {
+  const request_id = 1307;
+  afterEach((done) => {
+    sinon.restore();
+    done();
+  });
+
+  it("/download 200", async function () {
+    sinon.stub(s3, "getDownloadURL").resolves("sample_url");
+    sinon.stub(queryManager, "getPath").resolves("test_path");
+
+    const response = await supertest(app)
+      .get("/api/download")
+      .query({ req_id: request_id })
+      .expect(200);
+    expect(response.body).to.hasOwnProperty("success");
+    expect(response.body.s3_url).to.be.equal("sample_url");
+  });
+});
